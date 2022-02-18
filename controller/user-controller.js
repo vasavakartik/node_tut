a05892c2818e4a159f918f19a754753cbd2b8d17
vasavakartik <vasavakartikkumar@gmail.com>
2022-02-18 11:16:05 +0530

    user controller

---
+++ controller/user-controller.js
@@ -0,0 +1,73 @@
+const UserModel = require("../model/user-model")
+
+
+//add [ POST ]
+module.exports.addUser = function (req, res) {
+
+    let firstName = req.body.firstName
+    let email = req.body.email
+    let password = req.body.password
+    let role = req.body.role
+
+
+    let user = new UserModel({
+        firstName: firstName,
+        email: email,
+        password: password,
+        role: role
+    })
+
+
+
+    user.save(function (err, data) {
+        if (err) {
+            res.json({ msg: "SMW", data: err, status: -1 })//-1  [ 302 404 500 ]
+        } else {
+            res.json({ msg: "signup done", data: data, status: 200 })//http status code 
+        }
+    })
+
+
+}
+//list
+module.exports.getAllUsers = function (req, res) {
+
+    UserModel.find().populate("role").exec(function (err, data) {
+        if (err) {
+            res.json({ msg: "SMW", data: err, status: -1 })//-1  [ 302 404 500 ]
+        } else {
+            res.json({ msg: "users ret...", data: data, status: 200 })//http status code 
+        }
+    })
+}
+
+
+//delete
+module.exports.deleteUser = function(req,res){
+    //params userid 
+    let userId = req.params.userId //postman -> userid 
+
+    UserModel.deleteOne({_id:userId},function (err, data) {
+        if (err) {
+            res.json({ msg: "SMW", data: err, status: -1 })//-1  [ 302 404 500 ]
+        } else {
+            res.json({ msg: "user removed...", data: data, status: 200 })//http status code 
+        }
+    })
+}
+
+
+//update 
+module.exports.updateUser = function(req ,res){
+    let userId = req.body.userId
+    let firstName =req.body.firstName
+    UserModel.updateOne({_id:userId},{firstName:firstName},function(err,data){
+        if(err){
+            res.json({msg:"Something went wrong!!!",status:-1,data:err})
+        }else{
+            res.json({msg:"updated.",status:200,data:data})
+        }
+    })
+
+
+}
